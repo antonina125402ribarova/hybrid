3845a850f8c0779c9cb3b170e716e591294042ac
antonina125402ribarova <161422061+antonina125402ribarova@users.noreply.github.com>
2024-03-09 16:17:09 +0200

    v3

---
+++ advanced/04. Form Validation/formValidation.js
@@ -0,0 +1,61 @@
+//4
+function validate() {
+    $('#company').on('change', showHideCompany);
+
+    $('#submit').on('click', function(ev) {
+        ev.preventDefault();
+		
+        let usernameRegex = /^[a-zA-Z0-9]{3,20}$/;
+        let passwordRegex = /^\w{5,15}$/;
+        let emailRegex = /@.*\./;
+        let companyNumberRegex = /^[1-9]{1}[0-9]{3}$/;
+
+        let allFieldsValid = true;
+
+        if($('#username').value().match(usernameRegex)){
+            $('#username').css('border', 'none');
+        }else{
+            $('#username').css('border-color', 'red');
+            allFieldsValid = false;
+        }
+
+        if($('#email').value().match(emailRegex)){
+            $('#email').css('border', 'none');
+        }else{
+            $('#email').css('border-color', 'red');
+            allFieldsValid = false;
+        }
+
+        if($('#password').value().match(passwordRegex)){
+            $('#password').css('border', 'none');
+        }else{
+            $('#password').css('border-color', 'red');
+            allFieldsValid = false;
+        }
+
+        if($('#confirm-password').value().match(passwordRegex) && $('#confirm-password').val().localeCompare($('#password').val()) ==0){
+            $('#confirm-password').css('border', 'none');
+        }else{
+            $('#confirm-password').css('border-color', 'red');
+            allFieldsValid = false;
+        }
+
+        if($('#company').is('checked')){
+            if($('#companyNumber').value().match(companyNumberRegex)){
+            $('#companyNumber').css('border', 'none');
+        }else{
+            $('#companyNumber').css('border-color', 'red');
+            allFieldsValid = false;
+        }
+        }
+
+    });
+
+    function showHideCompany() {
+        if ($(this).is(':checked')) {
+            $('#companyInfo').css('display', 'block');
+        } else {
+            $('#companyInfo').css('display', 'none')
+        }
+    }
+}
